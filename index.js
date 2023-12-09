2be184d4c8c756a36a43739a2837892d777ffde9
Mahbub2027 <alam201127@gmail.com>
2023-12-09 12:16:35 +0600

    cart collection added

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
 
     const menuCollection = client.db('bistroDB').collection('menu');
     const reviewCollection = client.db('bistroDB').collection('reviews');
+    const cartCollection = client.db('bistroDB').collection('carts');
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray();
@@ -41,6 +42,19 @@ async function run() {
         res.send(result);
     })
 
+    // cart collection
+    app.post('/carts', async(req, res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
+    app.get('/carts', async(req, res)=>{
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray()
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
